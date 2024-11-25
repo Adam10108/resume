817048c8695e2fed4cb2150de1b1d75205e88be7
Adam10108 <adam_atlas1998@hotmail.com>
2024-11-25 08:13:46 +0700

    Running on port `3000`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+const serverPreviewSharedOptions = {
+  host: "0.0.0.0",
+  port: 3000,
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  server: { ...serverPreviewSharedOptions },
+  preview: { ...serverPreviewSharedOptions },
 })
